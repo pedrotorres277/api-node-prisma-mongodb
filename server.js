90dd421a6c575fd8d3cbd313563fc7ba14eff81b
pedrotorres277 <pedrolucast.2727@gmail.com>
2026-03-01 14:04:33 -0300

    Preparando para deploy no Render

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 const prisma = new PrismaClient();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.static(path.join(__dirname, "frontend")));
 
