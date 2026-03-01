ecb2f086d793eef60408b5ed974b3bebb811d346
pedrotorres277 <pedrolucast.2727@gmail.com>
2026-03-01 13:52:16 -0300

    API CRUD com Node, Express, Prisma e MongoDB

---
+++ Frontend/script.js
@@ -0,0 +1,49 @@
+const API_URL = "http://localhost:3000/users";
+
+async function loadUsers() {
+  const response = await fetch(API_URL);
+  const users = await response.json();
+
+  const list = document.getElementById("userList");
+  list.innerHTML = "";
+
+  users.forEach(user => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      ${user.name} - ${user.email} - ${user.age}
+      <button onclick="deleteUser('${user.id}')">Excluir</button>
+    `;
+    list.appendChild(li);
+  });
+}
+
+async function createUser() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const age = document.getElementById("age").value; // 👈 sem parseInt
+
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name, email, age })
+  });
+
+  if (response.ok) {
+    loadUsers();
+  } else {
+    const errorData = await response.json();
+    console.log(errorData);
+  }
+}
+
+async function deleteUser(id) {
+  await fetch(`${API_URL}/${id}`, {
+    method: "DELETE"
+  });
+
+  loadUsers();
+}
+
+loadUsers();
---
+++ server.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const prisma = new PrismaClient();
+const app = express();
+const PORT = 3000;
+app.use(cors());
+app.use(express.static(path.join(__dirname, "frontend")));
+
+// acessar o frontend sem precisar rodar o frentend separadamente
+
+app.use(express.json());
+
+app.post('/users', async (req, res) => {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                email: req.body.email,
+                name: req.body.name,
+                age: req.body.age
+            }
+        });
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: "Erro ao criar usuário", details: error.message });
+    }
+});
+
+app.put('/users/:id', async (req, res) => {
+    try {
+        const user = await prisma.user.update({
+            where: {
+                id: req.params.id
+            },
+            data: {
+                email: req.body.email,
+                name: req.body.name,
+                age: req.body.age
+            }
+        });
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: "Erro ao atualizar usuário", details: error.message });
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await prisma.user.delete({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ error: "Erro ao deletar usuário", details: error.message });
+    }
+});
+
+
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}/`);
+});
