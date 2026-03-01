1307a594a119544849d74dbda577ea87bf4a1d88
pedrotorres277 <pedrolucast.2727@gmail.com>
2026-03-01 14:17:59 -0300

    conexao do frontend com render atualizada

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.static(path.join(__dirname, "frontend")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+
+
 // acessar o frontend sem precisar rodar o frentend separadamente
 
 app.use(express.json());
